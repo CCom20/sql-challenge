b06169794955f4a12674c8e2e0e13328bef5d034
Cole <cole.d.comstock@gmail.com>
2020-11-05 20:49:23 -0700

    Finished up the dept employee view

--- employee_sql/query.sql
+++ employee_sql/query.sql
@@ -18,14 +18,35 @@ FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
 ORDER BY hire_date ASC
 
+SELECT * FROM emp_hire_date
+
 -- MANAGER DETAILS VIEW
 	-- List the manager of each department with the following information: 
 	-- department number, department name, the manager's employee number, last name, first name.
-	
+
+CREATE VIEW dept_manager_details AS
 SELECT dpt.dept_no, dpt.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM employees AS e
 JOIN dept_manager AS dm ON
 dm.emp_no = e.emp_no
 JOIN departments AS dpt ON
-dm.dept_no = dpt.dept_no	
-	
+dm.dept_no = dpt.dept_no
+
+SELECT * FROM dept_manager_details
+ORDER BY last_name ASC
+
+-- EMPLOYEE DEPT VIEW
+	-- List the department of each employee with the following information: 
+	-- employee number, last name, first name, and department name.
+
+CREATE VIEW emp_department AS
+SELECT e.emp_no, e.last_name, e.first_name, dept.dept_name
+FROM employees AS e
+JOIN dept_emp AS de ON
+e.emp_no = de.emp_no
+JOIN departments AS dept ON
+de.dept_no = dept.dept_no
+ORDER BY last_name ASC
+
+SELECT * FROM emp_department
+
