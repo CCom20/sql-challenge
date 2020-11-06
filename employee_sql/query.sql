83c510a45db09348bc278b55ac834482e7b5569c
Cole <cole.d.comstock@gmail.com>
2020-11-05 20:20:15 -0700

    Created hire_date view for employees

---
+++ employee_sql/query.sql
@@ -0,0 +1,18 @@
+-- EMPLOYEE DETAILS VIEW
+	-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+	
+CREATE VIEW employee_details AS
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+JOIN salaries AS s ON
+s.emp_no = e.emp_no
+
+SELECT * FROM employee_details
+
+-- DATE OF HIRE VIEW
+	-- List first name, last name, and hire date for employees who were hired in 1986.
+	
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY hire_date ASC
