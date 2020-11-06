bdf84a22431d460d861dfb04e420f4fa893e1963
Cole <cole.d.comstock@gmail.com>
2020-11-05 20:21:38 -0700

    created view for hire_date, named

--- employee_sql/query.sql
+++ employee_sql/query.sql
@@ -11,7 +11,8 @@ SELECT * FROM employee_details
 
 -- DATE OF HIRE VIEW
 	-- List first name, last name, and hire date for employees who were hired in 1986.
-	
+
+CREATE VIEW emp_hire_date AS
 SELECT first_name, last_name, hire_date 
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
