07b41f6082057979c48e8fd0024a042f074f1a7f
Cole Comstock <cole.d.comstock@gmail.com>
2020-11-09 15:36:16 -0700

    Added config file, should be block in github

--- employee_sql/create_tables.sql
+++ employee_sql/create_tables.sql
@@ -4,6 +4,24 @@ CREATE TABLE departments (
 	PRIMARY KEY (dept_no)
 );
 
+CREATE TABLE titles (
+	title_id VARCHAR,
+	title VARCHAR,
+	PRIMARY KEY (title_id)
+);
+
+CREATE TABLE employees (
+	emp_no INT,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
 CREATE TABLE dept_emp (
 	emp_no INT,
 	dept_no VARCHAR,
@@ -16,25 +34,6 @@ CREATE TABLE dept_manager (
 	emp_no INT,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
-
-CREATE TABLE titles (
-	title_id VARCHAR,
-	title VARCHAR,
-	PRIMARY KEY (title_id)
-);
-
-
-CREATE TABLE employees (
-	emp_no INT,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date DATE,
-	PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE salaries (
